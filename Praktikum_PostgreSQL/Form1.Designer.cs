93377eea560099f5775ae70328bf88811606f849
Alga <algavania454@gmail.com>
2023-05-12 23:52:22 +0700

    Add project files.

---
+++ Praktikum_PostgreSQL/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Praktikum_PostgreSQL
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbProduct = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.cmbCategory = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.txtProductName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnNew = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSave = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.guna2ContextMenuStrip1 = new Guna.UI2.WinForms.Guna2ContextMenuStrip();
+            this.txtDescription = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.BackColor = System.Drawing.Color.Transparent;
+            this.cmbProduct.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbProduct.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProduct.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbProduct.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbProduct.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbProduct.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cmbProduct.ItemHeight = 30;
+            this.cmbProduct.Location = new System.Drawing.Point(276, 95);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(408, 36);
+            this.cmbProduct.TabIndex = 0;
+            this.cmbProduct.SelectedIndexChanged += new System.EventHandler(this.onSelectedChange);
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.BackColor = System.Drawing.Color.Transparent;
+            this.cmbCategory.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbCategory.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbCategory.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cmbCategory.ItemHeight = 30;
+            this.cmbCategory.Location = new System.Drawing.Point(276, 221);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(408, 36);
+            this.cmbCategory.TabIndex = 1;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtProductName.DefaultText = "";
+            this.txtProductName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtProductName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtProductName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtProductName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtProductName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtProductName.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtProductName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtProductName.Location = new System.Drawing.Point(276, 289);
+            this.txtProductName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.PasswordChar = '\0';
+            this.txtProductName.PlaceholderText = "";
+            this.txtProductName.SelectedText = "";
+            this.txtProductName.Size = new System.Drawing.Size(408, 36);
+            this.txtProductName.TabIndex = 2;
+            // 
+            // btnNew
+            // 
+            this.btnNew.BorderRadius = 4;
+            this.btnNew.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNew.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnNew.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnNew.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnNew.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnNew.FillColor = System.Drawing.Color.Orange;
+            this.btnNew.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.ForeColor = System.Drawing.Color.White;
+            this.btnNew.Location = new System.Drawing.Point(36, 150);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(142, 45);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "Add New";
+            this.btnNew.Click += new System.EventHandler(this.onBtnNewClicked);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BorderRadius = 4;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSave.FillColor = System.Drawing.Color.Orange;
+            this.btnSave.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(36, 452);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(142, 45);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.onBtnSaveClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 35);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Product Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Find Product";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Product Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Description";
+            // 
+            // guna2ContextMenuStrip1
+            // 
+            this.guna2ContextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.guna2ContextMenuStrip1.Name = "guna2ContextMenuStrip1";
+            this.guna2ContextMenuStrip1.RenderStyle.ArrowColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.BorderColor = System.Drawing.Color.Gainsboro;
+            this.guna2ContextMenuStrip1.RenderStyle.ColorTable = null;
+            this.guna2ContextMenuStrip1.RenderStyle.RoundedEdges = true;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionArrowColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SeparatorColor = System.Drawing.Color.Gainsboro;
+            this.guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.guna2ContextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDescription.DefaultText = "";
+            this.txtDescription.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDescription.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDescription.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDescription.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDescription.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtDescription.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDescription.Location = new System.Drawing.Point(276, 356);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.PasswordChar = '\0';
+            this.txtDescription.PlaceholderText = "";
+            this.txtDescription.SelectedText = "";
+            this.txtDescription.Size = new System.Drawing.Size(408, 141);
+            this.txtDescription.TabIndex = 16;
+            // 
+            // ProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(856, 543);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.cmbProduct);
+            this.Name = "ProductForm";
+            this.Text = "Product Form";
+            this.Load += new System.EventHandler(this.onFormLoaded);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ComboBox cmbProduct;
+        private Guna.UI2.WinForms.Guna2ComboBox cmbCategory;
+        private Guna.UI2.WinForms.Guna2TextBox txtProductName;
+        private Guna.UI2.WinForms.Guna2Button btnNew;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2ContextMenuStrip guna2ContextMenuStrip1;
+        private Guna.UI2.WinForms.Guna2TextBox txtDescription;
+    }
+}
+
---
+++ Praktikum_PostgreSQL/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Windows.Forms.VisualStyles;
+
+namespace Praktikum_PostgreSQL
+{
+    public partial class ProductForm : Form
+    {
+        string connectionString = "DSN=Bakery;Uid=postgres;Pwd=password;";
+        List<ProductItem> productItems = new List<ProductItem>();
+        List<CategoryItem> categoryItems = new List<CategoryItem>();
+        bool isEdit = true;
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        private void onFormLoaded(object sender, EventArgs e)
+        {
+            OdbcConnection connection = new OdbcConnection(connectionString);
+            connection.Open();
+            loadProductComboBox(connection);
+            loadCategoryComboBox(connection);
+            connection.Close();
+        }
+
+        private void loadProductComboBox(OdbcConnection connection)
+        {
+            productItems.Clear();
+            OdbcCommand command = new OdbcCommand("SELECT * FROM public.\"Products Table\" ORDER BY \"ID\"", connection);
+            OdbcDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ProductItem item = new ProductItem(int.Parse(reader["ID"].ToString()), int.Parse(reader["Category ID"].ToString()), reader["Product Name"].ToString(), reader["Description"].ToString());
+                productItems.Add(item);
+            }
+            cmbProduct.DataSource = null;
+            cmbProduct.DataSource = productItems;
+            cmbProduct.DisplayMember = "ProductName";
+            cmbProduct.ValueMember = "ItemId";
+        }
+
+        private void loadCategoryComboBox(OdbcConnection connection)
+        {
+            categoryItems.Clear();
+            OdbcCommand command = new OdbcCommand("SELECT * FROM public.\"Categories\"", connection);
+            OdbcDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                CategoryItem item = new CategoryItem(int.Parse(reader["ID"].ToString()), reader["Product Types"].ToString());
+                categoryItems.Add(item);
+            }
+            cmbCategory.DataSource = categoryItems;
+            cmbCategory.DisplayMember = "ItemName";
+            cmbCategory.ValueMember = "ItemId";
+        }
+
+        private void onSelectedChange(object sender, EventArgs e)
+        {
+
+            if (cmbProduct.SelectedIndex != -1)
+            {
+                ProductItem product = productItems[cmbProduct.SelectedIndex];
+                cmbCategory.SelectedValue = product.CategoryId;
+                txtProductName.Text = product.ProductName;
+                txtDescription.Text = product.Description;
+            }
+        }
+
+        private void onBtnSaveClicked(object sender, EventArgs e)
+        {
+            string name = txtProductName.Text;
+            string description = txtDescription.Text;
+            if (name.Trim().Length > 0 && description.Trim().Length > 0 && cmbCategory.SelectedIndex != -1) 
+            {
+                OdbcConnection connection = new OdbcConnection(connectionString);
+                connection.Open();
+                name = name.Replace("'", "''");
+                description = description.Replace("'", "''");
+                string text;
+
+                if (cmbProduct.SelectedIndex != -1)
+                {
+                    isEdit = true;
+                }
+
+                if (isEdit)
+                {
+                    ProductItem product = productItems[cmbProduct.SelectedIndex];
+                    text = "UPDATE public.\"Products Table\" SET \"Category ID\"=" + cmbCategory.SelectedValue + ", \"Product Name\"='" + name + "', \"Description\"='" + description + "' WHERE \"ID\"=" + product.ItemId;
+                }
+                else
+                    text = "INSERT INTO public.\"Products Table\" (\"Category ID\", \"Product Name\", \"Description\") VALUES (" + cmbCategory.SelectedValue +", '"+name+"'"+ ", '"+description+"')";
+                OdbcCommand command = new OdbcCommand(text, connection);
+
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Product successfully saved!");
+                loadProductComboBox(connection);
+                if (!isEdit)
+                {
+                    string selectText = "SELECT lastval()";
+                    OdbcCommand selectCommand = new OdbcCommand(selectText, connection);
+                    int insertedId = Convert.ToInt32(selectCommand.ExecuteScalar());
+                    cmbProduct.SelectedValue = insertedId;
+                    isEdit = true;
+                }
+                connection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Field can't be empty!");
+            }
+        }
+
+        private void onBtnNewClicked(object sender, EventArgs e)
+        {
+            isEdit = false;
+            cmbProduct.SelectedItem = null;
+            cmbProduct.SelectedIndex = -1;
+            cmbCategory.SelectedItem = null;
+            cmbCategory.SelectedIndex = -1;
+            txtProductName.Text = "";
+            txtDescription.Text = "";
+        }
+    }
+
+    public class CategoryItem
+    {
+        public CategoryItem(int Id, string Name)
+        {
+            ItemId = Id;
+            ItemName = Name;
+        }
+
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+    }
+
+    public class ProductItem
+    {
+        public ProductItem(int Id, int CategoryId, string ProductName, string Description)
+        {
+            this.ItemId = Id;
+            this.CategoryId = CategoryId;
+            this.ProductName = ProductName;
+            this.Description = Description;
+        }
+
+        public int ItemId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+    }
+}
